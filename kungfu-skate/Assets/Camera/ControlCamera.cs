02a682dd04cc4a5161a3bc49c41a0535aaedbe26
Pau <103753396+pau-develop@users.noreply.github.com>
2023-01-08 11:25:03 +0100

    Created ControlCamera script

---
+++ kungfu-skate/Assets/Camera/ControlCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCamera : MonoBehaviour
+{
+    void Start()
+    {
+		if(Screen.fullScreen) Screen.SetResolution(1280, 720, true);
+		else Screen.SetResolution(1280, 720, false);  
+    }
+}
