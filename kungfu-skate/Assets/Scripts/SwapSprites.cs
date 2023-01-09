74237236f4601f3d0309f730f24b090cadb7605f
Pau <103753396+pau-develop@users.noreply.github.com>
2023-01-09 13:53:10 +0100

    Created SwapSprites script

---
+++ kungfu-skate/Assets/Scripts/SwapSprites.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SwapSprites : MonoBehaviour
+{
+    public string SpriteSheetName;
+    private string LoadedSpriteSheetName;
+    private Dictionary<string, Sprite> spriteSheet;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        LoadSpriteSheet();
+    }
+
+    private void LateUpdate()
+    {
+        if (this.LoadedSpriteSheetName != this.SpriteSheetName)
+        {
+            this.LoadSpriteSheet();
+        }
+        this.spriteRenderer.sprite = this.spriteSheet[this.spriteRenderer.sprite.name];
+    }
+
+    
+    private void LoadSpriteSheet()
+    {
+        var sprites = Resources.LoadAll<Sprite>("CHAR/"+this.SpriteSheetName);
+        this.spriteSheet = sprites.ToDictionary(x => x.name, x => x);
+        this.LoadedSpriteSheetName = this.SpriteSheetName;
+    }
+}
