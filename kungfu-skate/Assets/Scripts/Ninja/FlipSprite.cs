434be3fbb459617650fd8a3d7c4a4b1ee4fcc3a2
Pau <103753396+pau-develop@users.noreply.github.com>
2023-01-24 17:57:37 +0100

    Created function to track player position & flip sprite accordingly

---
+++ kungfu-skate/Assets/Scripts/Ninja/FlipSprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSprite : MonoBehaviour
+{
+    public bool isFliped;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFliped) flipSprite(true);
+        else flipSprite(false);
+    }
+
+    void flipSprite(bool fliped){
+        for(int i=0; i< transform.childCount;i++){
+            transform.GetChild(i).GetComponent<SpriteRenderer>().flipX = fliped;
+        }
+    }
+}
