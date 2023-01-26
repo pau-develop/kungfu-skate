7e609c9323f4b42c9466ca6150b4109e81e71a7d
Pau <103753396+pau-develop@users.noreply.github.com>
2023-01-26 19:55:35 +0100

    Particle movement

--- kungfu-skate/Assets/Scripts/Projectile/ProjectileCollision.cs
+++ kungfu-skate/Assets/Scripts/Projectile/ProjectileCollision.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class ProjectileCollision : MonoBehaviour
 {
     [SerializeField] private GameObject particle;
+    private int flipDirection;
 
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.tag =="Enemy") {
+            flipDirection = getProjectilePosition(collider);
             generateParticles();
             Destroy(this.gameObject);
         }
     }
 
+    int getProjectilePosition(Collider2D collider){
+        if(collider.transform.position.x < transform.position.x) return 1;
+        else return -1;
+    }
+
     void generateParticles(){
-        for(int i = 0; i < 6 ; i++){
+        Vector2[] directions = particle.GetComponent<ParticleMovement>().directions;
+        for(int i = 0; i < directions.Length ; i++){
             GameObject tempParticle = Instantiate(particle, transform.position, Quaternion.identity);
             tempParticle.GetComponent<SpriteRenderer>().color = new Color32(132,156,255,255);
+            tempParticle.GetComponent<ParticleMovement>().direction = directions[i];
+            tempParticle.GetComponent<ParticleMovement>().flipDirection = flipDirection;
         }
     }
 }
