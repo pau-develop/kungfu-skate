4a0a179538367e4aeafcfd21b5a1bc3f2bf3f02d
Pau <103753396+pau-develop@users.noreply.github.com>
2023-01-26 19:24:04 +0100

    Generating particles before destroying projectile

--- kungfu-skate/Assets/Scripts/Projectile/ProjectileCollision.cs
+++ kungfu-skate/Assets/Scripts/Projectile/ProjectileCollision.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class ProjectileCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject particle;
 
     void OnTriggerEnter2D(Collider2D collider){
-        if(collider.gameObject.tag =="Enemy") Destroy(this.gameObject);
+        if(collider.gameObject.tag =="Enemy") {
+            generateParticles();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void generateParticles(){
+        for(int i = 0; i < 6 ; i++){
+            GameObject tempParticle = Instantiate(particle, transform.position, Quaternion.identity);
+            tempParticle.GetComponent<SpriteRenderer>().color = new Color32(132,156,255,255);
+        }
     }
 }
