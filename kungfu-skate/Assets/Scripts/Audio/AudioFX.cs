f2216c0792cf6f7370e08b8ae4df2da2c54c5ec5
Pau <103753396+pau-develop@users.noreply.github.com>
2023-01-27 16:59:27 +0100

    Created AudioFX script with list of publi clips & a playSound function

---
+++ kungfu-skate/Assets/Scripts/Audio/AudioFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFX : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip char1Shoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void playSound(AudioClip currentAudio)
+    {
+        audioSource.PlayOneShot(currentAudio);
+    }
+}
--- kungfu-skate/Assets/Scripts/Character/Player/PlayerShoot.cs
+++ kungfu-skate/Assets/Scripts/Character/Player/PlayerShoot.cs
@@ -6,8 +6,13 @@ public class PlayerShoot : MonoBehaviour
 {
     public GameObject projectile;
     private Vector2 spawnLocation;
+    private AudioFX audioFX;
     // Start is called before the first frame update
+    void Start(){
+        audioFX = GameObject.Find("audio-fx").GetComponent<AudioFX>();
+    }
     void shootProjectile(){
+        audioFX.playSound(audioFX.char1Shoot);
         spawnLocation = new Vector2(transform.position.x+15,transform.position.y+20);
         GameObject tempProjectile = Instantiate(projectile,spawnLocation,Quaternion.identity);
     }
