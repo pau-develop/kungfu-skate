5302c3858486d028c639f1a376f8e893967216e3
Pau <103753396+pau-develop@users.noreply.github.com>
2023-02-08 18:57:11 +0100

    Checking if sprite is fliped and changing projectile spawn location accordingly

--- kungfu-skate/Assets/Scripts/Character/CharacterShoot.cs
+++ kungfu-skate/Assets/Scripts/Character/CharacterShoot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CharacterShoot : MonoBehaviour
 {
     public GameObject projectile;
-    private Vector2 spawnLocation;
+    public Vector2 spawnDistanceFromCenter;
     private AudioFX audioFX;
     private CharacterData charData;
     // Start is called before the first frame update
@@ -15,7 +15,16 @@ public class CharacterShoot : MonoBehaviour
     }
     void shootProjectile(){
         audioFX.playSound(charData.shoot);
-        spawnLocation = new Vector2(transform.position.x+15,transform.position.y+20);
+        int direction = getDirection();
+        Vector2 spawnLocation = new Vector2(transform.position.x+(spawnDistanceFromCenter.x*direction),transform.position.y+(spawnDistanceFromCenter.y*direction));
         GameObject tempProjectile = Instantiate(projectile,spawnLocation,Quaternion.identity);
+    }
+
+    int getDirection(){
+        if(transform.parent.GetComponent<FlipSprite>()){
+            if(transform.parent.GetComponent<FlipSprite>().isFliped) return -1;
+            return 1;
+        }
+        return 1; 
     }
 }
