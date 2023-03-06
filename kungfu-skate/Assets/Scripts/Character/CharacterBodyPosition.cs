f199a78647345b8ef2c1a8623edb7c6a574f3d35
Pau <103753396+pau-develop@users.noreply.github.com>
2023-03-06 10:29:01 +0100

    Made some changes to character suspensionEffect function

--- kungfu-skate/Assets/Scripts/Character/CharacterBodyPosition.cs
+++ kungfu-skate/Assets/Scripts/Character/CharacterBodyPosition.cs
@@ -10,8 +10,8 @@ public class CharacterBodyPosition : MonoBehaviour
     private GameObject playerBody;
     private Vector2 bodyDestPos;
     private Animator legsAnimator;
-    public int[] suspensionX;
-    public int[] suspensionY;
+    public float suspensionYLand;
+    public float suspensionYJump;
     public int[] groundPositions; //xForward, yForward, xBack, yBack
     public int[] airPositions; //xForward, yForward, xBack, yBack
  
@@ -32,26 +32,21 @@ public class CharacterBodyPosition : MonoBehaviour
     }
 
     void moveBodyParts(){
+        setBodyPosHorizontal();
         int currentState = checkAnimationState();
-        if(currentState == 0) suspensionEffect(-1);
-        else if(currentState == 1) suspensionEffect(1);
+        if(currentState == 0) suspensionEffect(suspensionYLand);
+        else if(currentState == 1) suspensionEffect(suspensionYJump);
         else {
             currentFrame = 0;
-            setBodyPos();
+            setBodyPosVertical();
         }
         playerBody.transform.position = bodyDestPos;
         playerArms.transform.position = bodyDestPos;
     }
 
 
-    void suspensionEffect(int direction){
-        currentFrame+=1;
-        int xDirection = getDirection();
-        if(currentFrame <=1) bodyDestPos = new Vector2(transform.position.x+suspensionX[0]*xDirection,transform.position.y+suspensionY[1] * direction);
-        else if(currentFrame > 2) bodyDestPos = new Vector2(transform.position.x+suspensionX[1]*xDirection,transform.position.y+suspensionY[2] * direction);
-        else if(currentFrame > 3) bodyDestPos = new Vector2(transform.position.x+suspensionX[2]*xDirection,transform.position.y+suspensionY[3] * direction);
-        else if(currentFrame > 4) bodyDestPos = new Vector2(transform.position.x+suspensionX[3]*xDirection,transform.position.y+suspensionY[2] * direction);
-        else if(currentFrame > 5) bodyDestPos = new Vector2(transform.position.x+suspensionX[4]*xDirection,transform.position.y+suspensionY[1] * direction);
+    void suspensionEffect(float suspensionY){
+        bodyDestPos.y = transform.position.y+suspensionY;
     }
 
     int checkAnimationState(){
@@ -62,19 +57,32 @@ public class CharacterBodyPosition : MonoBehaviour
         else if(currentAnim == "legs-jump") return 1;
         else return 2;
     }
-    
 
-    void setBodyPos(){
+    void setBodyPosHorizontal(){
         int direction = getDirection();
         if(legsAnimator.GetBool("isGrounded")){
-            if(legsAnimator.GetBool("movingBack")) bodyDestPos = new Vector2(transform.position.x+groundPositions[2]*direction,transform.position.y+groundPositions[3]);
-            else if(legsAnimator.GetBool("movingForward")) bodyDestPos = new Vector2(transform.position.x+groundPositions[0]*direction,transform.position.y+groundPositions[1]); 
-            else bodyDestPos = new Vector2(transform.position.x*direction,transform.position.y+1);
+            if(legsAnimator.GetBool("movingBack")) bodyDestPos.x = transform.position.x+groundPositions[2]*direction;
+            else if(legsAnimator.GetBool("movingForward")) bodyDestPos.x = transform.position.x+groundPositions[0]*direction; 
+            else bodyDestPos.x = transform.position.x*direction;
         }
         else{
-            if(legsAnimator.GetBool("movingBack")) bodyDestPos = new Vector2(transform.position.x+airPositions[2]*direction,transform.position.y+airPositions[3]);
-            else if(legsAnimator.GetBool("movingForward")) bodyDestPos = new Vector2(transform.position.x+airPositions[0]*direction,transform.position.y+airPositions[1]);
-            else bodyDestPos = new Vector2(transform.position.x,transform.position.y);
+            if(legsAnimator.GetBool("movingBack")) bodyDestPos.x = transform.position.x+airPositions[2]*direction;
+            else if(legsAnimator.GetBool("movingForward")) bodyDestPos.x = transform.position.x+airPositions[0]*direction;
+            else bodyDestPos.x = transform.position.x;
+        }
+    }
+
+    void setBodyPosVertical(){
+        int direction = getDirection();
+        if(legsAnimator.GetBool("isGrounded")){
+            if(legsAnimator.GetBool("movingBack")) bodyDestPos.y = transform.position.y+groundPositions[3];
+            else if(legsAnimator.GetBool("movingForward")) bodyDestPos.y = transform.position.y+groundPositions[1]; 
+            else bodyDestPos.y = transform.position.y+1;
+        }
+        else{
+            if(legsAnimator.GetBool("movingBack")) bodyDestPos.y = transform.position.y+airPositions[3];
+            else if(legsAnimator.GetBool("movingForward")) bodyDestPos.y = transform.position.y+airPositions[1];
+            else bodyDestPos.y = transform.position.y;
         }
     }
 
