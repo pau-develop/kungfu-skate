07eb469cbef11605714eb177d93557f1c860a0ad
Pau <103753396+pau-develop@users.noreply.github.com>
2023-03-26 13:12:10 +0200

    Created wave prefab and wave movement function

---
+++ kungfu-skate/Assets/Scripts/Projectile/Wave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    private int direction = 1;
+    private Vector2 wavePos; 
+    private int waveSpeed = 150;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wavePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveWave();
+    }
+
+    void moveWave(){
+        wavePos.x += (waveSpeed * direction) * Time.deltaTime; 
+        transform.position = wavePos;
+    }
+}
