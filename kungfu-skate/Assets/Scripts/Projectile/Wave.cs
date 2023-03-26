2f7b7c4cb142eba6d5953d1850a3ead68c00d6e8
Pau <103753396+pau-develop@users.noreply.github.com>
2023-03-26 13:15:41 +0200

    Created destroyWave function

--- kungfu-skate/Assets/Scripts/Projectile/Wave.cs
+++ kungfu-skate/Assets/Scripts/Projectile/Wave.cs
@@ -7,6 +7,8 @@ public class Wave : MonoBehaviour
     private int direction = 1;
     private Vector2 wavePos; 
     private int waveSpeed = 150;
+    private int leftLimit = -175;
+    private int rightLimit = 175;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +19,16 @@ public class Wave : MonoBehaviour
     void Update()
     {
         moveWave();
+        destroyWave();
     }
 
     void moveWave(){
         wavePos.x += (waveSpeed * direction) * Time.deltaTime; 
         transform.position = wavePos;
+    }
+
+    void destroyWave(){
+        if(direction == 1 && wavePos.x > rightLimit) Destroy(this.gameObject);
+        if(direction == -1 && wavePos.x < leftLimit) Destroy(this.gameObject);
     }
 }
