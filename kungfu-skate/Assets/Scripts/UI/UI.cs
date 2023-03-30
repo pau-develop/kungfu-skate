486d2235b576aa1d8497c7e4e3f9db15b4125dc7
Pau <103753396+pau-develop@users.noreply.github.com>
2023-03-30 19:44:03 +0200

    Displaying FPS on screen

---
+++ kungfu-skate/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI : MonoBehaviour
+{
+    private GameObject debugger;
+    private TextMeshPro fpsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        debugger = transform.Find("debugger").gameObject;
+        fpsText =  debugger.transform.Find("fps").GetComponent<TextMeshPro>();
+        Debug.Log(fpsText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countFrames();   
+    }
+
+    private void countFrames(){
+		float fps = (1 / Time.unscaledDeltaTime);
+		fpsText.text = "FPS: " + (int)fps;
+	}
+}
