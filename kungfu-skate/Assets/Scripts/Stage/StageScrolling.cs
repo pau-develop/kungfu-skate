af864d36d1c20a7d989e4a61bd59285838e6d7ff
Pau <103753396+pau-develop@users.noreply.github.com>
2023-03-30 20:21:36 +0200

    Adding background sprites to array

---
+++ kungfu-skate/Assets/Scripts/Stage/StageScrolling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageScrolling : MonoBehaviour
+{
+    public Sprite[] sprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
