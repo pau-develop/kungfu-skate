f69dc44982b7008243af0399c1777bef0f16c304
Pau <103753396+pau-develop@users.noreply.github.com>
2023-03-31 00:06:40 +0200

    Displaying time spent on scene in UI

--- kungfu-skate/Assets/Scripts/UI/UI.cs
+++ kungfu-skate/Assets/Scripts/UI/UI.cs
@@ -8,14 +8,17 @@ public class UI : MonoBehaviour
     private GameObject debugger;
     private TextMeshPro fpsText;
     private TextMeshPro scrollXText;
+    private TextMeshPro timeText;
     public int scrollX;
     private bool isDisplaying = false;
+    private float timePassed = 0;
     // Start is called before the first frame update
     void Start()
     {
         debugger = transform.Find("debugger").gameObject;
         fpsText =  debugger.transform.Find("fps").GetComponent<TextMeshPro>();
         scrollXText =  debugger.transform.Find("scrollX").GetComponent<TextMeshPro>();
+        timeText =  debugger.transform.Find("time").GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
@@ -23,7 +26,8 @@ public class UI : MonoBehaviour
     {
         displayDebugger();
         countFrames();
-        displayScrollX();   
+        displayScrollX();
+        displayTimeOnScene();   
     }
 
     private void displayDebugger(){
@@ -34,10 +38,18 @@ public class UI : MonoBehaviour
 
     private void countFrames(){
 		float fps = (1 / Time.unscaledDeltaTime);
-		fpsText.text = "FPS: " + (int)fps;
+		fpsText.text = "FPS:  " + (int)fps;
 	}
 
     private void displayScrollX(){
         scrollXText.text = "xPos: " + scrollX;
+    }
+
+    private void displayTimeOnScene(){
+        timePassed += Time.deltaTime;
+        string secondsPassed = ((int)timePassed%60).ToString();
+        if(secondsPassed.Length == 1) secondsPassed = "0" + secondsPassed;
+        int minutesPassed = (int)Mathf.Round(timePassed/60);
+        timeText.text = "Time: " + minutesPassed + ":" + secondsPassed;
     }
 }
