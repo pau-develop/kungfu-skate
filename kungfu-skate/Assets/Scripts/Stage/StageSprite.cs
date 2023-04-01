d95e92274ad755c3d927f1c9a4f910945bd43241
Pau <103753396+pau-develop@users.noreply.github.com>
2023-04-01 11:17:52 +0200

    Changing color indexes for each color cycle and saving as a new sprite

--- kungfu-skate/Assets/Scripts/Stage/StageSprite.cs
+++ kungfu-skate/Assets/Scripts/Stage/StageSprite.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class StageSprite : MonoBehaviour
 {
     private Texture2D originalTexture;
+    public Sprite[] newSprites; //as many sprites as colorCycles in ColorList
     private Color32[] colorArray;
     public Color32[] colorsToChange;
     private List<List<int>> colorIndexes = new List<List<int>>();
-    private ColorList newColors; 
+    private ColorList newColors;
     // Start is called before the first frame update
     void Start()
     {
         newColors = GetComponent<ColorLister>().colorList;
         originalTexture = GetComponent<SpriteRenderer>().sprite.texture;
-		colorArray = originalTexture.GetPixels32();
+        newSprites = new Sprite[newColors.colorCycles.Count];
+        colorArray = originalTexture.GetPixels32();
         storeColorIndexes();
-        foreach(Color32 color in newColors.colorCycles[0].newColor) Debug.Log(color);
+        createTextureWithNewColors();
+        // foreach(Color32 color in newColors.colorCycles[0].newColor) Debug.Log(color);
     }
 
     private void storeColorIndexes(){
@@ -32,6 +35,24 @@ public class StageSprite : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void createTextureWithNewColors(){
+        for(int z = 0; z < newSprites.Length; z++){
+            for(int i = 0; i < colorIndexes.Count; i++){
+                for(int y = 0; y < colorIndexes[i].Count; y++){
+                    colorArray[colorIndexes[i][y]].r = newColors.colorCycles[z].newColor[i].r;
+                    colorArray[colorIndexes[i][y]].g = newColors.colorCycles[z].newColor[i].g;
+                    colorArray[colorIndexes[i][y]].b = newColors.colorCycles[z].newColor[i].b;
+                }
+            }
+            Texture2D tempTexture;
+            tempTexture =  new Texture2D(originalTexture.width, originalTexture.height, TextureFormat.RGBA32, true);
+            tempTexture.SetPixels32(colorArray);
+            tempTexture.filterMode = FilterMode.Point;
+            tempTexture.Apply();
+            newSprites[z] = Sprite.Create(tempTexture, new Rect(0,0,640,180),new Vector2(0.5f,0.5f),1);
+        }   
     }
 
     // Update is called once per frame
