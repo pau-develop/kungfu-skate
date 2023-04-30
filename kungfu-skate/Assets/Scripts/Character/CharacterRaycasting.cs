d09185b49121bc6dcc978fdea1fb41615fe0c6ff
Pau <103753396+pau-develop@users.noreply.github.com>
2023-04-30 17:37:33 +0200

    Adding colliders to background pieces and returning collider pos after raycasting

--- kungfu-skate/Assets/Scripts/Character/CharacterRaycasting.cs
+++ kungfu-skate/Assets/Scripts/Character/CharacterRaycasting.cs
@@ -17,14 +17,27 @@ public class CharacterRaycasting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        doTheRayCasting(leftRayXPos);
-        doTheRayCasting(rightRayXPos);
+        castRays();
     }
 
-    private void doTheRayCasting(int rayOrigin){
+
+    private void castRays(){
+        int leftBottom = (int)doTheRayCasting(leftRayXPos);
+        int rightBottom = (int)doTheRayCasting(rightRayXPos);
+        Debug.Log(leftBottom);
+        Debug.Log(rightBottom);
+    }
+
+    private float doTheRayCasting(int rayOrigin){
         Vector2 rayPos = new Vector2(transform.position.x + rayOrigin, transform.position.y + 5);
         RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.down, rayLength);
         Debug.DrawRay(rayPos, Vector2.down * rayLength, Color.green);
-        if(hit.collider != null) Debug.Log(hit.collider);
+        if(hit.collider != null) return getColliderPosition(hit.collider);
+        else return -160;
+    }
+
+    private float getColliderPosition(Collider2D collider){
+        BoxCollider2D boxCollider = collider.GetComponent<BoxCollider2D>();
+        return boxCollider.offset.y;
     }
 }
