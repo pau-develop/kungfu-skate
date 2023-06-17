3a26d66777da4a067dbb97b13a56b7ef5db67ba7
Pau <103753396+pau-develop@users.noreply.github.com>
2023-06-17 10:54:52 +0200

    Moving blood particle to initial dest position right after spawning

--- kungfu-skate/Assets/Scripts/Character/CharacterBloodParticles.cs
+++ kungfu-skate/Assets/Scripts/Character/CharacterBloodParticles.cs
@@ -9,7 +9,12 @@ public class CharacterBloodParticles : MonoBehaviour
     public void spawnBloodParticles(){
         Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + 20);
         int bloodDensity = Random.Range(100,200);
-        for(int i = 0; i < bloodDensity; i++)
-            Instantiate(bloodParticle, spawnPosition, Quaternion.identity);
+        GameObject tempParticle;
+        for(int i = 0; i < bloodDensity; i++){
+            tempParticle = Instantiate(bloodParticle, spawnPosition, Quaternion.identity);
+            if(GetComponent<FlipSprite>() != null)
+                tempParticle.GetComponent<BloodParticleMovement>().isFliped = GetComponent<FlipSprite>().isFliped;
+            else tempParticle.GetComponent<BloodParticleMovement>().isFliped = false; 
+        }
     }
 }
--- kungfu-skate/Assets/Scripts/Particle/BloodParticleMovement.cs
+++ kungfu-skate/Assets/Scripts/Particle/BloodParticleMovement.cs
@@ -8,7 +8,12 @@ public class BloodParticleMovement : MonoBehaviour
     private SpriteRenderer bloodRenderer;
     private int fallSpeed;
     private float dropSize;
+    private Vector2 initialOriginPos;
     private Vector2 initialDestPos;
+    private Vector2 height;
+    private bool reachedInitialDestPos = false;
+    private float count = 0;
+    public bool isFliped = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +21,9 @@ public class BloodParticleMovement : MonoBehaviour
 		fallSpeed = Random.Range(10,40) * 10;
 		dropSize = Random.Range(1.0f,2.0f);
         setColor();
+        initialOriginPos = transform.position;
         initialDestPos = getInitialDestPos();
+        height = initialOriginPos +(initialDestPos - initialOriginPos)/2 + Vector2.up *Random.Range(1.0f,40.0f);
     }
 
     private void setColor(){
@@ -25,16 +32,24 @@ public class BloodParticleMovement : MonoBehaviour
     }
 
     private Vector2 getInitialDestPos(){
-        bool isFliped = GetComponent<FlipSprite>().isFliped;
         int direction;
-        if(isFliped) direction = 1;
-        else direction = -1;
-        return new Vector2(transform.position.x-Random.Range(5.0f * direction, 10.0f * direction),transform.position.y);
+        if(isFliped) direction = -1;
+        else direction = 1;
+        return new Vector2(transform.position.x - Random.Range(5.0f * direction, 20.0f * direction),transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!reachedInitialDestPos) moveParticle();
+    }
+
+    private void moveParticle(){
+		if (count < 0.75f) {
+			count += 1.0f *Time.deltaTime*4f;
+			Vector2 m1 = Vector2.Lerp( initialOriginPos, height, count );
+			Vector2 m2 = Vector2.Lerp( height, initialDestPos, count );
+			transform.position = Vector2.Lerp(m1, m2, count);
+		} else reachedInitialDestPos = true;
     }
 }
