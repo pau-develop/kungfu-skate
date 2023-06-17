8bee975fea19685cb80b66bc8979459524203cad
Pau <103753396+pau-develop@users.noreply.github.com>
2023-06-17 11:49:23 +0200

    Detecting bottom position & blood particle fall movement

--- kungfu-skate/Assets/Scripts/Particle/BloodParticleMovement.cs
+++ kungfu-skate/Assets/Scripts/Particle/BloodParticleMovement.cs
@@ -11,9 +11,12 @@ public class BloodParticleMovement : MonoBehaviour
     private Vector2 initialOriginPos;
     private Vector2 initialDestPos;
     private Vector2 height;
+    private Vector2 bloodPos;
     private bool reachedInitialDestPos = false;
     private float count = 0;
     public bool isFliped = false;
+    private bool hasFallen = false;
+    private int botPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +27,23 @@ public class BloodParticleMovement : MonoBehaviour
         initialOriginPos = transform.position;
         initialDestPos = getInitialDestPos();
         height = initialOriginPos +(initialDestPos - initialOriginPos)/2 + Vector2.up *Random.Range(1.0f,40.0f);
+        botPosition = getBottomPosition();
+    }
+
+    private int getBottomPosition(){
+        //PENDING
+        int rayLength = 180;
+        Vector2 rayPos = transform.position;
+        Debug.DrawRay(rayPos, Vector2.down * rayLength, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.down, rayLength);
+        if(hit.collider != null && hit.collider.tag == "Obstacle"){
+            float screenBottom = -90;
+            BoxCollider2D boxCollider = hit.collider.GetComponent<BoxCollider2D>();
+            int originalBottomPosition = (int)(screenBottom + boxCollider.size.y + 3);
+            int threshold = 10;
+            return Random.Range(originalBottomPosition + threshold, originalBottomPosition - threshold);
+        }
+        return -180;
     }
 
     private void setColor(){
@@ -35,16 +55,26 @@ public class BloodParticleMovement : MonoBehaviour
         int direction;
         if(isFliped) direction = -1;
         else direction = 1;
-        return new Vector2(transform.position.x - Random.Range(5.0f * direction, 20.0f * direction),transform.position.y);
+        return new Vector2(transform.position.x - Random.Range(5.0f * direction, 30.0f * direction),transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!reachedInitialDestPos) moveParticle();
+        if(!reachedInitialDestPos) particleInitialMovement();
+        if(reachedInitialDestPos && !hasFallen) particleFallMovement();
     }
 
-    private void moveParticle(){
+    private void particleFallMovement(){
+		if(transform.position.y > botPosition){
+			bloodPos = transform.position;
+			bloodPos.y -= fallSpeed * Time.deltaTime;
+			bloodPos.x -= fallSpeed/10* Time.deltaTime;
+			transform.position = bloodPos;
+		} else hasFallen = true;
+    }
+
+    private void particleInitialMovement(){
 		if (count < 0.75f) {
 			count += 1.0f *Time.deltaTime*4f;
 			Vector2 m1 = Vector2.Lerp( initialOriginPos, height, count );
