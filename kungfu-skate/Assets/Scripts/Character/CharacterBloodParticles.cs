e75df4e3a8d1a62dd3178a464fa0d9f515b272ff
Pau <103753396+pau-develop@users.noreply.github.com>
2023-06-17 10:11:48 +0200

    Instantiating blood particles on key press

---
+++ kungfu-skate/Assets/Scripts/Character/CharacterBloodParticles.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBloodParticles : MonoBehaviour
+{
+    [SerializeField] private GameObject bloodParticle;
+    // Start is called before the first frame update
+    public void spawnBloodParticles(){
+        Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + 20);
+        int bloodDensity = Random.Range(100,200);
+        for(int i = 0; i < bloodDensity; i++)
+            Instantiate(bloodParticle, spawnPosition, Quaternion.identity);
+    }
+}
