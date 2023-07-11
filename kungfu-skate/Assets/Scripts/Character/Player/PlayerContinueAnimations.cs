9aa08eaf53e34ccf561174cf39dd23b5aaf0fbc2
Pau <103753396+pau-develop@users.noreply.github.com>
2023-07-11 10:42:29 +0200

    Playing random animations in continue screen

--- kungfu-skate/Assets/Scripts/Character/Player/PlayerContinueAnimations.cs
+++ kungfu-skate/Assets/Scripts/Character/Player/PlayerContinueAnimations.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PlayerContinueAnimations : MonoBehaviour
 {
-    
+    private string[] headAnimationsNames = new string[]{
+        "continue-head-cry",
+        "continue-head-hurt",
+    };
+    private Animator headAnimator;
+    private float animationSpeed = 1;
+    private bool isPlaying;
     // Start is called before the first frame update
     void Start()
     {
-        
+        headAnimator = this.transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(headAnimator.GetCurrentAnimatorStateInfo(0).IsName("null")) playRandomAnimation();
+    }
+
+    private void playRandomAnimation(){
+        int randomNumber = Random.Range(0, headAnimationsNames.Length);
+        headAnimator.Play(headAnimationsNames[randomNumber]);
     }
 }
